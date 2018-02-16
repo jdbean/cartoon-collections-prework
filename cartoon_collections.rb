307b4cc603a04e18b590f34e210fd4dccb3703a4
J.D. Bean <jonathandbean@gmail.com>
2018-02-16 01:04:09 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,6 +17,6 @@ end
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.find do |item|
-    cheese_types.incudes?(item)
+    cheese_types.incude?(item)
   end
 end
